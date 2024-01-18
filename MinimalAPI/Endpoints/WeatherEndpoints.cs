3e8da60c46a2b830bcf05d53c517f7e779c9e8cd
Todor <84274078+TLizhev@users.noreply.github.com>
2024-01-18 15:51:43 +0000

    Add project files.

---
+++ MinimalAPI/Endpoints/WeatherEndpoints.cs
@@ -0,0 +1,29 @@
+﻿using MinimalAPI.Models;
+
+namespace MinimalAPI.Endpoints;
+
+public static class WeatherEndpoints
+{
+    public static void AddWeatherEndpoints(this IEndpointRouteBuilder app)
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", (HttpContext _) =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        (
+                            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            Random.Shared.Next(-20, 55),
+                            summaries[Random.Shared.Next(summaries.Length)]
+                        ))
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+    }
+}
